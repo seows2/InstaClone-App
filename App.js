7167236381b4411fad6e721d8651400e158a0a65
서우석 <seows@seouseog-ui-MacBook-Pro.local>
2020-03-15 02:15:41 +0900

    first Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
